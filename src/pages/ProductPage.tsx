f0e731038d4c4f130a78ae5c13da433aa5305abe
Szymon <sgrzywna@griddynamics.com>
2025-04-01 16:49:27 +0200

    Convert product fetch from json-server request to regular filter

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,36 +1,37 @@
 
 import { Skeleton } from "@mui/material";
 import { useParams } from "react-router-dom";
-import { useProductCodeFetch } from "../hooks/useProductCodeFetch";
 import ProductDescription from "../sections/Product/ProductDescription/ProductDescription";
 import ProductHero from "../sections/Product/ProductHero/ProductHero";
 import RelatedProducts from "../sections/Product/RelatedProducts/RelatedProducts";
 import Error from "../sections/Error/Error";
+import { useContext } from "react";
+import { ProductContext } from "../store/ProductContext";
 
 const ProductPage: React.FC = () => {
     const params = useParams();
-    const {productData, isLoading, error} = useProductCodeFetch(params.productCode ?? '');  
+    const {productData, isLoading, error} = useContext(ProductContext);
   
     if (error) return <Error message={error} />
     
-    const [product] = productData ?? [];
+    const product = productData?.find(product => product.code === params.productCode);
+
+    if (isLoading && !product) {
+        return (
+            <div style={{ display: "flex", justifyContent: "space-around", padding: "5rem" }}>
+                <Skeleton variant="rectangular" sx={{ width: "40vw", height: "30vw", borderRadius: "2rem" }} />
+                <Skeleton variant="rectangular" sx={{ width: "40vw", height: "30vw", borderRadius: "2rem" }} />
+            </div>
+        );
+    }
+
+    if (!product) return <Error message="Product not found" />;
 
     return (
         <>
-            {
-                isLoading ? (
-                    <div style={{display: "flex", justifyContent: "space-around", padding: "5rem"}}>
-                        <Skeleton variant="rectangular" sx={{ width: "40vw", height: "30vw", borderRadius: "2rem"}} />
-                        <Skeleton variant="rectangular" sx={{ width: "40vw", height: "30vw", borderRadius: "2rem"}} />
-                    </div>
-                ) : (
-                    <>
-                        <ProductHero {...product}/>
-                        <ProductDescription {...product} />
-                        <RelatedProducts />
-                    </>
-                )
-            }
+            <ProductHero {...product} />
+            <ProductDescription {...product} />
+            <RelatedProducts />
         </>
     );
 }
